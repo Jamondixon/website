484f73d268fae2db9e4949b4d76d203e7b0df852
Jamon Dixon <jamonadixon@gmail.com>
2021-08-31 19:44:14 -0500

    app setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+import {Switch, Route, useLocation} from 'react-router-dom';
+import Home from './Pages/Home/Home';
+import About from './Pages/About/About';
+import Resume from './Pages/Resume/Resume';
+import Projects from './Pages/Projects/Projects';
+import Blogs from './Pages/Blogs/Blogs';
+import Contact from './Pages/Contact/Contact';
 import './App.css';
 
 function App() {
+
+  let location = useLocation();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Switch location={location} key={location.pathname}>
+        <Route path='/' exact component={Home} />
+        <Route path='/about' exact component={About} />
+        <Route path='/resume' exact component={Resume} />
+        <Route path='/projects' exact component={Projects} />
+        <Route path='/blogs' exact component={Blogs} />
+        <Route path='/contact' exact component={Contact} />
+      </Switch>
+    </>
   );
 }
 
