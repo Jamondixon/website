6d377c1b07755227eb5ddf993ff7e81b79281ee0
Jamon Dixon <jamonadixon@gmail.com>
2021-09-09 15:23:19 -0500

    skills list

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -18,10 +18,28 @@ const About = () => {
                     <div className='text-container'>
                         <div className='first-body'>
                             <div className='paragraph-container'>
-                                <h4 className='about-subtitle'>BACKGROUND</h4>
+                                <h2 className='about-subtitle'>BACKGROUND</h2>
                                 <p className='about-body'>
                                 Full-stack web developer with a curiosity of what makes things work and an aptitude for problem solving. I'm an avid sports fan and love to go see some live music with my fiance, Ellie. A regular at music festivals all over the United States and you can always find me at the disc golf course in my free time.
                                 </p>
+                            </div>
+                            <div className="padding-div">
+                                <div className="skill-container">
+                                    <div className="skill-heading-container">
+                                        <h2 className="skill-title">SKILLS</h2>
+                                        <p className="skill-paragraph">
+                                            <ul className="skill-list">
+                                                <li className='skill-list-item'>HTML</li>
+                                                <li className='skill-list-item'>CSS</li>
+                                                <li className='skill-list-item'>Ruby</li>
+                                                <li className='skill-list-item'>Rails</li>
+                                                <li className='skill-list-item'>Javascript</li>
+                                                <li className='skill-list-item'>React</li>
+                                                <li className='skill-list-item'>React Native</li>
+                                            </ul>
+                                        </p>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
