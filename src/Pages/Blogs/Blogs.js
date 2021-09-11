87fffbb3fab206d258756dcca5e9f43f141272d5
Jamon Dixon <jamonadixon@gmail.com>
2021-09-11 13:55:44 -0500

    blogs template

--- src/Pages/Blogs/Blogs.js
+++ src/Pages/Blogs/Blogs.js
@@ -3,7 +3,35 @@ import './Blogs.css'
 
 const Blogs = () => {
     return (
-        <Nav />
+        <div className="blogs-overall-container">
+            <Nav />
+            <div className="blogs-container">
+                <div className="blogs-title-container">
+                    <h1 className="blogs-title">BLOGS</h1>
+                </div>
+                <div className='card-container'>
+                    <div className="card">
+                        <div className="card-title-container">
+                            <h2 className="card-title"></h2>
+                            <a className="card-link"
+                            href=''
+                            target='_blank'
+                            rel='noopener noreferrer'
+                            >
+                                <button className="card-button" 
+                                buttonStyle='primary--btn'>READ</button>
+                            </a>
+                        </div>
+                        <div className="card-image-container">
+                            <img className="card-image" src='' alt='blog image' />
+                        </div>
+                        <div className="card-date-container">
+                            <h3 className="card-date"></h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 };
 
