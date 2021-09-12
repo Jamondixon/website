57f03ee1800b5dc9acb533d258f19fbb6dc383ec
Jamon Dixon <jamonadixon@gmail.com>
2021-09-11 19:05:14 -0500

    projects images

--- src/Pages/Projects/Projects.js
+++ src/Pages/Projects/Projects.js
@@ -9,47 +9,61 @@ const Projects = () => {
                 <div className="projects-title-container">
                     <h1 className="projects-title">PROJECTS</h1>
                 </div>
-                <div className="card-container">
-                    <div className="card">
-                        <div className="card-title-container">
-                            <h2 className="card-title">C-S-Sexy</h2>
-                            <a className="card-link"
-                            href='https://jamondixon.medium.com/c-s-sexy-576c7a377691'
+                <div className="projects-card-container">
+                    <div className="projects-card">
+                        <div className="projects-card-title-container">
+                            <h2 className="projects-card-title">303 QUESOFINDER</h2>
+                            <a className="projects-card-link"
+                            href='https://hopeful-raman-70b1e5.netlify.app/'
                             target='_blank'
                             rel='noopener noreferrer'
                             >
-                                <button className="card-button" 
+                                <button className="projects-card-button" 
                                 buttonStyle='primary--btn'>CHECK IT OUT!</button>
                             </a>
-                            <h3 className="card-date">May 9th, 2021</h3>
+                            <h3 className="projects-card-description">SPA designed to showcase, rate, and point people in the direction of Denver’s best queso</h3>
                         </div>
                     </div>
-                    <div className="card two">
-                        <div className="card-title-container">
-                            <h2 className="card-title">An ode to JSX</h2>
-                            <a className="card-link"
-                            href='https://medium.com/nerd-for-tech/an-ode-to-jsk-85f6ce8e0a6b'
+                    <div className="projects-card-two">
+                        <div className="projects-card-title-container">
+                            <h2 className="projects-card-title">CryptoCurrent</h2>
+                            <a className="projects-card-link"
+                            href='https://gracious-kilby-8e4aa8.netlify.app/'
                             target='_blank'
                             rel='noopener noreferrer'
                             >
-                                <button className="card-button" 
+                                <button className="projects-card-button" 
                                 buttonStyle='primary--btn'>CHECK IT OUT!</button>
                             </a>
-                            <h3 className="card-date">July 19th, 2021</h3>
+                            <h3 className="projects-card-description">JavaScript application to track cryptocurrency prices in real time with graphical representation, favorites list and calendar time search features.</h3>
                         </div>
                     </div>
-                    <div className="card three">
-                        <div className="card-title-container">
-                            <h2 className="card-title">Hooks and Ladders Pt. 1</h2>
-                            <a className="card-link"
-                            href='https://medium.com/nerd-for-tech/hooks-and-ladders-8d4f708b054d'
+                    <div className="projects-card-three">
+                        <div className="projects-card-title-container">
+                            <h2 className="projects-card-title">Forgotten Friend</h2>
+                            <a className="projects-card-link"
+                            href='https://github.com/Jamondixon/ForgottenFriend'
                             target='_blank'
                             rel='noopener noreferrer'
                             >
-                                <button className="card-button" 
+                                <button className="projects-card-button" 
                                 buttonStyle='primary--btn'>CHECK IT OUT!</button>
                             </a>
-                            <h3 className="card-date">July 26th, 2021</h3>
+                            <h3 className="projects-card-description">React Native mobile app to help people connect with others at the same event to help them find items they may have forgotten after arrival.</h3>
+                        </div>
+                    </div>
+                    <div className="projects-card-four">
+                        <div className="projects-card-title-container">
+                            <h2 className="projects-card-title">Just Fling It</h2>
+                            <a className="projects-card-link"
+                            href='https://awesome-hypatia-163c1c.netlify.app/'
+                            target='_blank'
+                            rel='noopener noreferrer'
+                            >
+                                <button className="projects-card-button" 
+                                buttonStyle='primary--btn'>CHECK IT OUT!</button>
+                            </a>
+                            <h3 className="projects-card-description">Website to help connect disc golfers with the best custon dyed discs</h3>
                         </div>
                     </div>
                 </div>
