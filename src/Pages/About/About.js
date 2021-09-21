1515b255037b7c9a01b2c20e88d0cd1dafb4ef52
Jamon Dixon <jamonadixon@gmail.com>
2021-09-21 13:45:04 -0500

    about page

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -2,7 +2,8 @@ import React from 'react';
 import Nav from '../../Components/Nav/NavBar';
 import './About.css';
 import Profile from '../../Images/headshotcropped1.png'
-import Queso from '../../Images/pretty2jamon1.jpg'
+import Queso from '../../Images/prettyjamon1.jpg'
+import Skills from '../../Images/skillscropped.png'
 
 const About = () => {
     return (
@@ -18,7 +19,8 @@ const About = () => {
                 <div className="padding-div">
                     <div className="skill-container">
                         <div className="skill-heading-container">
-                            <h2 className="skill-title">SKILLS</h2>
+                            {/* <h2 className="skill-title">SKILLS</h2> */}
+                            <img src={Skills} className="skill-title" alt="skills" />
                             <p className="skill-paragraph">
                                 <ul className="skill-list">
                                     <li className='skill-list-item'>HTML</li>
@@ -42,16 +44,15 @@ const About = () => {
                                 Full-stack web developer with a curiosity of what makes things work and an aptitude for problem solving. Interested in fostering creativity and a sense of purpose. A graduate of University Studies from Texas Tech University, I consider myself a jack of all trades. My experience ranges all over the map, from restaurant and hospitality to lanscaping and most recently cannabis. With the heart of an entrepreneur, I am always hungry for the next challenge.   
                             </p>
                         </div>
+                        <div className="queso-image-container">
+                            <img className="queso-image" src={Queso} alt="queso scene" />
+                        </div>
                         <div className='personal-paragraph-container'>
                             <h2 className='about-subtitle'>PERSONAL LIFE</h2>
                             <p className='about-body'>
                                 I'm an avid sports fan and love to go see some live music with my fiancé, Ellie. Lover of good food and drink. I'm a regular at music festivals all over the United States and you can always find me at the disc golf course in my free time. When I am not out and about I enjoy curling up on the couch to watch a good movie with my fluffy, black, rescue cat named Artemis. 
                             </p>
                         </div>
-                        <div className="queso-image-container">
-                            <img className="queso-image" src={Queso} alt="queso scene" />
-                        </div>
-                        
                     </div>
                 </div>
             </div>
