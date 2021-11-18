5322cb8818f6b785b7dfdd926c733582af950a43
Jamon Dixon <jamonadixon@gmail.com>
2021-11-17 20:03:37 -0600

    contact change

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -144,33 +144,33 @@ const Contact = (props) => {
                         <h3 className="contact-phone-title">PHONE</h3>
                         {/* <h4 className="contact-phone-value">806-620-6896</h4> */}
                         <img className="phone-value" src={Phone} alt="phone" />
-                    <div className="contact-icons-container">
-                        <div className="contact-icons-inner-container">
-                            <a href="https://www.linkedin.com/in/jamondixon/" 
-                            rel="noreferrer" 
-                            target="_blank"
-                            >
-                                <img className="contact-social-icon" src={LinkedIn} alt="LinkedIn Icon"/>
-                            </a>
-                            <a href="https://github.com/Jamondixon" 
-                            rel="noreferrer" 
-                            target="_blank"
-                            >
-                                <img className="contact-social-icon" src={Github} alt="Github Icon"/>
-                            </a>
-                            <a href="https://www.facebook.com/jamon.dixon" 
-                            rel="noreferrer" 
-                            target="_blank"
-                            >
-                                <img className="contact-social-icon" src={Facebook} alt="Facebook Icon"/>
-                            </a>
-                            <a href="https://jamondixon.medium.com/" 
-                            rel="noreferrer" 
-                            target="_blank"
-                            >
-                                <img className="contact-social-icon" src={Medium} alt="Medium Icon"/>
-                            </a>
-                        </div>
+                </div>
+                <div className="contact-icons-container">
+                    <div className="contact-icons-inner-container">
+                        <a href="https://www.linkedin.com/in/jamondixon/" 
+                        rel="noreferrer" 
+                        target="_blank"
+                        >
+                            <img className="contact-social-icon" src={LinkedIn} alt="LinkedIn Icon"/>
+                        </a>
+                        <a href="https://github.com/Jamondixon" 
+                        rel="noreferrer" 
+                        target="_blank"
+                        >
+                            <img className="contact-social-icon" src={Github} alt="Github Icon"/>
+                        </a>
+                        <a href="https://www.facebook.com/jamon.dixon" 
+                        rel="noreferrer" 
+                        target="_blank"
+                        >
+                            <img className="contact-social-icon" src={Facebook} alt="Facebook Icon"/>
+                        </a>
+                        <a href="https://jamondixon.medium.com/" 
+                        rel="noreferrer" 
+                        target="_blank"
+                        >
+                            <img className="contact-social-icon" src={Medium} alt="Medium Icon"/>
+                        </a>
                     </div>
                 </div>
             </div>
